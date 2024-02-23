fc31ada652112031fe8fd4a53946f5aaaaeff5f3
Nikhil-Mindpath <nikhil.pathade@mindpath.com>
2024-02-23 18:12:55 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -35,3 +35,15 @@ const app  = express()
 
 
 connectDB()
+.then(()=>{
+  app.on("error",(error)=>{
+    console.log("ERROR: ",error);
+    throw error ;
+  })
+ app.listen(process.env.PORT || 8000,()=>{
+  console.log(` Server is running at port ${process.env.PORT}`);
+ })
+})
+.catch((err)=>{
+  console.log("Mongo db connection failed !!! ",err);
+})
